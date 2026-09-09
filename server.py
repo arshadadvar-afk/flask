99ca43f2596c76ee96049e5936b913840b1781c2
Arsha <arsha.dadvar@gmail.com>
2026-09-09 11:05:34 +0330

    Add files via upload

---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+data = [
+    ['admin', '05519218']
+]
+
+
+def ai (q):
+    if q:
+        return 'من یک هوش مصتوعی هستم و هنوز آماده برای کمک به شما نشده ام !'
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+@app.route('/login', methods=["get"])
+def login_page():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['post'])
+def login():
+    name = request.form['username']
+    password = request.form['password']
+    for i in data:
+        if name == i[0] and password == i[1]:
+            return redirect('/page')
+        elif name == '' and password == '':
+            return '<h1>please enter something !</h1>'
+    else:
+        return render_template('bad_login.html')
+
+@app.route('/page', methods=['GET', 'POST'])
+def page():
+    answer = ''
+    if request.method == 'POST':
+        message = request.form['message']
+        answer = ai(message)
+    return render_template('page.html', answer=answer)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
